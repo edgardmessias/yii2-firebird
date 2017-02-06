0700082a6e4cacddd5b529d9ae114e5ec067e3e2
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-02-06 15:37:27 -0200

    Added tests for Firebird 3.0;

--- tests/ci/travis/setup_firebird_2.1.sh
+++ tests/ci/travis/setup_firebird_2.1.sh
@@ -2,7 +2,7 @@
 
 apt-get install -qq firebird2.1-super firebird2.1-dev expect
 export DEBIAN_FRONTEND=readline
-expect ${TRAVIS_BUILD_DIR}/tests/ci/travis/dpkg_firebird2.1.exp
+expect ${TRAVIS_BUILD_DIR}/tests/ci/travis/dpkg_firebird_2.1.exp
 export DEBIAN_FRONTEND=dialog
 
 service firebird2.1-super start
---
+++ tests/ci/travis/setup_firebird_3.0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+echo " ... Downloading source"
+git clone --depth=1 -b B3_0_Release --single-branch https://github.com/FirebirdSQL/firebird.git
+
+echo " ... Preparing source"
+cd firebird
+apt-get install -qq expect docbook docbook-to-man libatomic-ops-dev libbsd-dev libedit-dev libsp1c2 sgml-data sp libtommath-dev
+./autogen.sh
+
+echo " ... Compiling source"
+make -j `nproc`
+
+echo " ... Installing"
+export DEBIAN_FRONTEND=readline
+expect ${TRAVIS_BUILD_DIR}/tests/ci/travis/dpkg_firebird_3.0.exp
+export DEBIAN_FRONTEND=dialog
+
+ln -s /usr/local/firebird/bin/isql /usr/local/firebird/bin/isql-fb
+
+echo " ... Starting Firebird 3.0"
+service firebird start
