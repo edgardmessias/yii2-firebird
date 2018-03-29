5e5904d426e21db91c38c9cf5b1e9896662fd17c
Edgard Lorraine Messias <edgardmessias@gmail.com>
2018-03-29 09:57:18 -0300

    Fixed build of firebird 2.5

--- tests/ci/travis/setup_firebird_2.5.sh
+++ tests/ci/travis/setup_firebird_2.5.sh
@@ -3,6 +3,6 @@
 apt-get install -qq firebird2.5-super firebird2.5-dev
 export FIREBIRD_SERVER_CONFIG=/etc/default/firebird$FB
 sed /ENABLE_FIREBIRD_SERVER=/s/no/yes/ -i $FIREBIRD_SERVER_CONFIG
-car $FIREBIRD_SERVER_CONFIG | grep ENABLE_FIREBIRD_SERVER
+cat $FIREBIRD_SERVER_CONFIG | grep ENABLE_FIREBIRD_SERVER
 
 service firebird2.5-super start
